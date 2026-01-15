bb348722b62b2c69bed774151e9e92542a75d787
23120319 <truongthanhphat1103@gmail.com>
2026-01-15 22:20:53 +0700

    feat(login): add login controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import authRoute from "./routes/auth.route.js";
 import errorHandler from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,38 @@
+import AuthService from "../services/auth.service.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 export const AuthController = {
 
+    async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
 
+            if (!email || !password) {
+                throw new Error("Email and password are required");
+            }
+
+            const { data: user, error } = await AuthService.login(email, password);
+            if (error) {
+                throw error;
+            }
+
+            res.cookie("token", user.token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "lax",
+                maxAge: 24 * 60 * 60 * 1000
+            });
+
+            res.json({
+                user: user.user
+            });
+
+        } catch (error) {
+            next(error);
+        }
+    }
 
 
 }
