775ff5a7a36ed79e9b9820560d936670ad04417b
23120319 <truongthanhphat1103@gmail.com>
2026-01-15 21:41:18 +0700

    feat(login): add login service, add column state into users table

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,51 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+export const AuthService = {
+    async login(email, password) {
+        try {
+            // check existed user
+            const { data: user, error } = await User.findUserByEmail(email);
+            if (error || !user) {
+                throw new Error("User not found");
+            }
+
+            // check user state
+            if (user.state !== "active") {
+                throw new Error("User is locked");
+            }
+
+            // check password
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if (!isPasswordValid) {
+                throw new Error("Invalid password");
+            }
+            // generate token
+            const token = jwt.sign(
+                {
+                    id: user._id,
+                    email: user.email,
+                    role: user.role
+                },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h" }
+            );
+
+            return {
+                token,
+                user: {
+                    id: user._id,
+                    email: user.email,
+                    role: user.role
+                }
+            };
+        } catch (error) {
+            next(error);
+        }
+    },
+
+
+}
+
