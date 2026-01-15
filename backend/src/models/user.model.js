f32e0f71fca17236a85078f694ea18e2777daec2
23120319 <truongthanhphat1103@gmail.com>
2026-01-15 22:09:36 +0700

    feat(login):login model interact with supabase

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,17 @@
+
+export const UserModel = {
+
+    async findUserByEmail(email) {
+        try {
+            const user = await knex("users")
+                .where({ email })
+                .first();
+
+            return { data: user, error: null };
+        } catch (error) {
+            return { data: null, error };
+        }
+    },
+
+}
+
