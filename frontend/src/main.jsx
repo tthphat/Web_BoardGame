db8f424f9c41deed1f7af40022b9b579639fe2d2
23120328 <buuhuynhvinhphuc@gmail.com>
2026-01-15 17:07:50 +0700

    fix: conflict and resolve missing page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom' // 1. Đổi cái này
-import App from './App' // 2. Import App của bạn
-import AuthProvider from '@/contexts/AuthContext'
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 3. Dùng BrowserRouter bọc bên ngoài cùng */}
-    <BrowserRouter>
-      <AuthProvider>
-        <App /> {/* 4. Gọi App vào đây */}
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,7 +1,51 @@
-import LoginPage from "@/pages/auth/LoginPage";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import MainLayout from "../components/common/MainLayout";
+import DashboardPage from "../pages/DashboardPage";
+import DemoPage from "../pages/DemoPage";
+// import LoginPage from "../pages/auth/LoginPage";
+// import RegisterPage from "../pages/auth/RegisterPage";
 
 export const router = createBrowserRouter([
-    { path: "/login", element: <LoginPage /> },
-    // { path: "/register", element: <Register /> },
-])
+    // {
+    //     path: "/login",
+    //     element: <LoginPage />
+    // },
+    // {
+    //     path: "/register",
+    //     element: <RegisterPage />
+    // },
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Navigate to="/dashboard" replace />
+            },
+            {
+                path: "/dashboard",
+                element: <DashboardPage />
+            },
+            {
+                path: "/profile",
+                element: <DemoPage title="Profile" description="Đây là trang thông tin cá nhân. Hãy thử cuộn chuột hoặc đổi theme!" />
+            },
+            {
+                path: "/friends",
+                element: <DemoPage title="Friends List" description="Danh sách bạn bè sẽ hiện ở đây." />
+            },
+            {
+                path: "/messages",
+                element: <DemoPage title="Messages" description="Hộp thư đến 0 tin nhắn mới." />
+            },
+            {
+                path: "/trophy",
+                element: <DemoPage title="Achievements" description="Các danh hiệu bạn đã đạt được." />
+            },
+            {
+                path: "/ranking",
+                element: <DemoPage title="Ranking" description="Bảng xếp hạng toàn server." />
+            }
+        ]
+    }
+]);
