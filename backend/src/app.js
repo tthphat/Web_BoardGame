475975809317751f517d79262fcb58aae19718c6
23120328 <buuhuynhvinhphuc@gmail.com>
2026-01-15 21:20:14 +0700

    feat(api): import swagger for api docs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,36 @@
 import express from "express";
 import cors from "cors";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Swagger Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Health check
+ *     description: Returns a simple message to verify the server is running
+ *     responses:
+ *       200:
+ *         description: Server is up and running
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello World
+ */
+app.get('/', (req, res) => {
+    res.json({ message: "Hello World" });
+});
 
 export default app;
