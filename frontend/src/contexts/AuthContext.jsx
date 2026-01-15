788ba750dc1289487b16aa95c5ad41da40810aa7
23120319 <truongthanhphat1103@gmail.com>
2026-01-15 14:46:40 +0700

    feat(login): create AuthContext

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,27 @@
+import { useContext, createContext } from "react";
+
+const AuthContext = createContext(null);
+
+function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const login = async (payload) => {
+        try {
+            const response = await loginApi(payload);
+            setUser(response.data.user);
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthProvider;
+
+export const useAuth = () => { useContext(AuthContext) };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/routes'
+import { router } from './routes/routes';
+import AuthProvider from '@/contexts/AuthContext';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
