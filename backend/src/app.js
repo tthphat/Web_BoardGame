619b02b0bb90106a91282d468bbe751870dc196d
23120319 <truongthanhphat1103@gmail.com>
2026-01-18 14:33:13 +0700

    feat(backend): implement check api key

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,7 @@ import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
+import apiKeyMiddleware from "./middlewares/apiKey.middleware.js";
 
 
 const app = express();
@@ -20,7 +21,9 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoute); // login, register không cần check api key
+
+app.use(apiKeyMiddleware); // middleware kiểm tra api key
 app.use("/api/achievements", achievementRoute);
 app.use("/api/games", gameRoute);
 app.use("/api/user", userRoute);
