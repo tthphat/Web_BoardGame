16064964dded3af9de7af749d8271f4332e89bb7
23120319 <truongthanhphat1103@gmail.com>
2026-01-20 14:47:33 +0700

    feat(deploy): fix error board config export name

--- frontend/src/utils/boardConfig.js
+++ frontend/src/utils/boardConfig.js
@@ -27,6 +27,6 @@ export const BOARD_SIZES = {
 // Hardcode hiện tại - sau này Admin sẽ chỉnh từ DB
 export const CURRENT_BOARD_SIZE = 'EXTRA_LARGE';
 
-export const agetBoardConfig = () => {
+export const getBoardConfig = () => {
   return BOARD_SIZES[CURRENT_BOARD_SIZE];
 };
